f45fbac5df5f96b9b4e931729346083bd5241ea6
JudySays.Nihao <judysays.nihao@gmail.com>
2023-04-22 19:36:53 +0800

    Update 2023-04-22T11:36:50.769Z

--- app/assets/js/all.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello!');
-console.log('你好!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log('Hello!');
+console.log('你好!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+//# sourceMappingURL=all.js.map
